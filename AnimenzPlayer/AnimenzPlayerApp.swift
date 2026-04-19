859a770529a4e18c09c4be0bf4f6887c567a5ca0
AgainTri412 <nullptragaintri@gmail.com>
2026-04-19 05:47:05 -0400

    Initial Commit

---
+++ AnimenzPlayer/AnimenzPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimenzPlayerApp.swift
+//  AnimenzPlayer
+//
+//  Created by albert einstein on 4/19/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimenzPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
